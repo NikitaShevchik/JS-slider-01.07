711d6fbb0e35d1b8a148055449f4300ad493fa1c
NikitaShevchik <shevchik.nikita@gmail.com>
2022-07-01 20:51:57 +0300

    changer 2sec

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,1 +1,23 @@
 "use strict"
+
+const textSlider = document.querySelector('.slider__text');
+
+let text = ['Привет, я искусственный интелект.', 'Меня закрыли внутри этого блока.', 'Помогите...']
+
+
+
+//textSlider.innerHTML = k
+
+function changeText() {
+    let i = 0
+    textSlider.innerHTML = text[i]
+    function counter() {
+        textSlider.innerHTML = text[i]
+        i++;
+        if (i == text.length) {
+            i = 0;
+        }
+    }
+    setInterval(counter, 2000)
+}
+changeText()
